881ca0c738df1be1f9a9ba7a14bea3c742a9797f
Timothy jan <jan.timothy@gmail.com>
2024-05-22 17:19:07 -0700

    Punch Screen updated per postpunch documentation

--- src/app/components/fk-components/punch-screen/punch-screen.component.ts
+++ src/app/components/fk-components/punch-screen/punch-screen.component.ts
@@ -9,12 +9,10 @@ import { JantekService } from '../../../services/jantek.service';
 })
 export class PunchScreenComponent implements OnInit{
   clocktype: string = "";
-  currentDateTime = new Date();
+  currentDateTime:Date = new Date();
   username: string = "";
   punchForm: FormGroup = new FormGroup({
-    user: new FormControl({value: "", disabled: true}, Validators.required),
-    punch: new FormControl("", Validators.required),
-    currentDateTime: new FormControl("", Validators.required),
+    punchcode: new FormControl("", Validators.required),
   });
 
   constructor(
@@ -22,16 +20,20 @@ export class PunchScreenComponent implements OnInit{
   ){}
 
   ngOnInit(): void {
-    this.username = this._jantekService.employeeStatus.firstname + " " + this._jantekService.employeeStatus.lastname;
-    /** Needed to get the punchconfig including clocktype */
-    this._jantekService.getPunchConfiguration();
-    this.clocktype = this.FkClockTypeDesc(
-      this._jantekService.getClockType()
-    );
+    this.setUsername();
+    /** Get Punch Configuration to get clocktype*/
+    this._jantekService.getPunchConfiguration().subscribe(response => {
+      this.clocktype = this.FkClockTypeDesc(response.clocktype);
+    });
   }
 
+  /** Set username */
+  setUsername(): void {
+    this.username = this._jantekService.employeeStatus.firstname + " " + this._jantekService.employeeStatus.lastname;
+  }
+
+  /** Returns clock type for the NgSwitch */
   FkClockTypeDesc(clocktype:Number): string {
-    /** Returns clock type for the NgSwitch */
     var desc = "";
     switch (clocktype) {
       case 1:
@@ -49,28 +51,40 @@ export class PunchScreenComponent implements OnInit{
     return desc;
   }
 
+  /** Sets PunchCode in Punchform to 'IN' */
+  setPunchCodeIn(): void {
+    this.punchForm.controls['punchcode'].setValue("IN");
+  }
+
+  /** Sets PunchCode in Punchform to 'OUT' */
+  setPunchCodeOut(): void {
+    this.punchForm.controls['punchcode'].setValue("OUT");
+  }
+
+  /** Sets PunchCode in Punchform to 'SJ' which is 'Swipe and Go' */
+  setPunchCodeSJ(): void {
+    this.punchForm.controls['punchcode'].setValue("SJ");
+  }
+
+  /** Post Punch In */
   onPunchIn() {
-    this.punchForm.controls['punch'].setValue("IN");
-    this.currentDateTime = new Date();
-    this.punchForm.controls['currentDateTime'].setValue(this.currentDateTime);
+    this.setPunchCodeIn();
     if (this.punchForm.valid) {
       this._jantekService.postPunch(this.punchForm.value);
     }
   }
 
+  /** Post Punch Out */
   onPunchOut() {
-    this.punchForm.controls['punch'].setValue("OUT:");
-    this.currentDateTime = new Date();
-    this.punchForm.controls['currentDateTime'].setValue(this.currentDateTime);
+    this.setPunchCodeOut();
     if (this.punchForm.valid) {
       this._jantekService.postPunch(this.punchForm.value);
     }
   }
 
+  /** Post Punch Swipe and Go */
   onPunchSwipeAndGo() {
-    this.punchForm.controls['punch'].setValue("SWIPEANDGO:", );
-    this.currentDateTime = new Date();
-    this.punchForm.controls['currentDateTime'].setValue(this.currentDateTime);
+    this.setPunchCodeSJ
     if (this.punchForm.valid) {
       this._jantekService.postPunch(this.punchForm.value);
     }
